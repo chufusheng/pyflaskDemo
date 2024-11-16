9d776138600ebb52fdd2d2f6be0f84bb6474d6b1
qiuhaitang <qiuhaitang@wacai.com>
2024-11-16 22:48:20 +0800

    1

--- log_config.py
+++ log_config.py
@@ -5,12 +5,11 @@ from datetime import datetime
 
 
 def setup_logging():
-    log_dir = '../logs'
+    log_dir = 'logs'
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-    # 根据当前时间生成日志文件名称
-    current_time = datetime.now()
-    log_file_name = os.path.join(log_dir, f'app_{current_time.strftime("%Y%m%d%H%M")}.log')
+
+    log_file_name = os.path.join(log_dir, 'app.log')
     log_handler = RotatingFileHandler(log_file_name, maxBytes=1024 * 1024, backupCount=5)
     log_handler.setLevel(logging.INFO)
     log_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
--- routes/ai_test.py
+++ routes/ai_test.py
@@ -1,25 +1,11 @@
-from flask import Blueprint, render_template, request,send_from_directory
+from flask import Blueprint, render_template, request
 
-from db.sql_manager import SQLManager
-from config.log_config import setup_logging
+from log_config import setup_logging
 
-logger = setup_logging()
+log = setup_logging()
 
 ai = Blueprint('ai', __name__)
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
 
 #
 # @ai.route('/')  # 首页路由
@@ -32,73 +18,73 @@ def hello_world():
     return render_template('index.html')
 
 
-
-@ai.route('/upload', methods=['GET', 'POST'])
-def upload_test():
-    if request.method == 'GET':
-        return render_template('ai/photo.html', data='')
-    else:
-        f = request.files['photo']
-        type = request.form.get("l_type")
-        print(type)
-        txt = image_to_words(f, type)
-        return render_template('ai/photo.html', data=txt)
-
-
-@ai.route('/up_photo', methods=['GET', 'POST'], strict_slashes=False)
-def up_photo():
-    f = request.files['photo']
-    txt = image_to_words(f)
-    return render_template('ai/photo.html', data=txt)
-
-
-@ai.route('/ai_chat', methods=['GET', 'POST'], strict_slashes=False)
-def ai_chat():
-    oldRes = request.args.get("oldRes")
-    msg = request.args.get("msg")
-    res = voice_chat(oldRes, msg)
-    return res
-
-
-@ai.route('/ai_chat_index', methods=['GET', 'POST'], strict_slashes=False)
-def ai_chat_index():
-    return render_template('ai/chat.html')
+#
+# @ai.route('/upload', methods=['GET', 'POST'])
+# def upload_test():
+#     if request.method == 'GET':
+#         return render_template('ai/photo.html', data='')
+#     else:
+#         f = request.files['photo']
+#         type = request.form.get("l_type")
+#         print(type)
+#         txt = image_to_words(f, type)
+#         return render_template('ai/photo.html', data=txt)
+#
+#
+# @ai.route('/up_photo', methods=['GET', 'POST'], strict_slashes=False)
+# def up_photo():
+#     f = request.files['photo']
+#     txt = image_to_words(f)
+#     return render_template('ai/photo.html', data=txt)
+#
+#
+# @ai.route('/ai_chat', methods=['GET', 'POST'], strict_slashes=False)
+# def ai_chat():
+#     oldRes = request.args.get("oldRes")
+#     msg = request.args.get("msg")
+#     res = voice_chat(oldRes, msg)
+#     return res
 
 
-def storing_statistical():
-    db = SQLManager("3dthing")
+# @ai.route('/ai_chat_index', methods=['GET', 'POST'], strict_slashes=False)
+# def ai_chat_index():
+#     return render_template('ai/chat.html')
+#
 
-    page_count = 10000
-    sql = "select id,file_size from things_files order by id desc limit {0}, {1};"
-    mb_count = 0
-    kb_count = 0
-    for page in range(1, 51):
-        print(page)
-        list = db.get_list(sql.format((page - 1) * page_count, page_count))
-        for i in list:
-            try:
-                data = str(i['file_size']).split(' ')
-                num = int(data[0])
-                unit = data[1]
-                if unit.__contains__('mb'):
-                    mb_count = mb_count + num
-                if unit.__contains__('kb'):
-                    kb_count = kb_count + num
-            except Exception as e:
-                print("-----")
-                print(i["id"])
-
-    print(mb_count * 1024 + kb_count)
-
+# def storing_statistical():
+#     db = SQLManager("3dthing")
+#
+#     page_count = 10000
+#     sql = "select id,file_size from things_files order by id desc limit {0}, {1};"
+#     mb_count = 0
+#     kb_count = 0
+#     for page in range(1, 51):
+#         print(page)
+#         list = db.get_list(sql.format((page - 1) * page_count, page_count))
+#         for i in list:
+#             try:
+#                 data = str(i['file_size']).split(' ')
+#                 num = int(data[0])
+#                 unit = data[1]
+#                 if unit.__contains__('mb'):
+#                     mb_count = mb_count + num
+#                 if unit.__contains__('kb'):
+#                     kb_count = kb_count + num
+#             except Exception as e:
+#                 print("-----")
+#                 print(i["id"])
+#
+#     print(mb_count * 1024 + kb_count)
+#
 
 @ai.route('/payres', methods=['GET', 'POST'], strict_slashes=False)  # 首页路由
 def payres():
     data = request.get_json()
-    logger.info(data)
-    logger.info("@ai.route('/payres', methods=['GET', 'POST'], strict_slashes=False)  # 首页路由")
+    log.info(data)
+    log.debug("@ai.route('/payres', methods=['GET', 'POST'], strict_slashes=False)  # 首页路由")
     print(data)
     return "success"
 
-
-if __name__ == '__main__':
-    storing_statistical()
+#
+# if __name__ == '__main__':
+#     storing_statistical()
