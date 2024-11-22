adeb861e11030390918f70abfedacc7e56e88fee
qiuhaitang <qiuhaitang@wacai.com>
2024-11-22 15:13:44 +0800

    order_init

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 aikelai_pay = {
     "appid": "1345",
     "key": "n636Oj66g2609Kj9z9uN9njUj9OZ80aJ",
-    "notify_url": "http://124.223.168.99:5000/ai/payres",
-    "return_url": "http://124.223.168.99:5000/ai",
+    "notify_url": "http://124.223.168.99:5000/pay/callBack",
+    "return_url": "http://47.98.45.112:8001/#/pages/order/order",
 }
 
 doubao_ai = {
